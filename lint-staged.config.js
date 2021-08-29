13442b50d2296c4a01fe0efbdbae91f12065072a
Antonio Thomas <antoniothomasjr@gmail.com>
2021-08-29 11:41:09 -0500

    testing husky

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'*.{js,ts,tsx}': ['eslint --fix'],
-	'*.+(js|jsx|json|yml|yaml|css|less|css|scss |ts|tsx| md|graphql|mdx)': [
-		'prettier --write',
-	],
-}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,6 +2,7 @@ import Footer from 'components/footer'
 import Header from 'components/header'
 import MobileHeader from 'components/mobile-header'
 import React from 'react'
+
 interface LayoutProps {
 	children: React.ReactNode
 }
