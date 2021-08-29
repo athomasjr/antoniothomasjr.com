bb2190241c14fe42790648c0a0c38129615011c0
Antonio Thomas <antoniothomasjr@gmail.com>
2021-08-29 11:12:11 -0500

    test husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'*.{js,ts,tsx}': ['eslint'],
+	'*.{js,ts,tsx}': ['eslint --fix'],
 	'*.+(js|jsx|json|yml|yaml|css|less|css|scss |ts|tsx| md|graphql|mdx)': [
 		'prettier --write',
 	],
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,11 +2,9 @@ import Footer from 'components/footer'
 import Header from 'components/header'
 import MobileHeader from 'components/mobile-header'
 import React from 'react'
-
 interface LayoutProps {
 	children: React.ReactNode
 }
-
 export default function Layout({ children }: LayoutProps) {
 	return (
 		<div>
